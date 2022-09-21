fb5f15db618be52bcf43ec25347b0d77742d4167
mehdi-jarrar <89733518+mehdi-jarrar@users.noreply.github.com>
2022-09-22 00:49:08 +0100

    first commit

---
+++ src/components/Products/Update/validators.js
@@ -0,0 +1,14 @@
+import {timestampToDays} from '../../../utils';
+
+export const isNameValid = (value) => {
+	return value.trim().length > 0 && value.trim().length <= 200;
+};
+
+export const isCategoriesValid = (value) => {
+	return value.length > 0 && value.length <= 5;
+}
+export const isExpirationDateValid = (value) => {
+	var minExpirationDate = new Date() ; 
+	minExpirationDate.setDate(minExpirationDate.getDate() + 30 )
+	return  minExpirationDate <new Date(value) ; 
+}
